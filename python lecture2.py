ffdd949575e931e6af7a8250c40bdb38322331d0
Malenfores <156945943+Malenfores@users.noreply.github.com>
2025-09-22 05:45:14 -0700

    string properties
    indexing, Slicing
    next line string
    tap line string
    length of string

---
+++ python lecture2.py
@@ -0,0 +1,38 @@
+# This is a next line string
+
+str1="This is a string.\n We are creating it in python."
+print(str1)
+
+# This is a tab line string
+
+str2="This is a string.\t We are creating it in python."
+print(str2)
+
+# concatenate string mean sum of string
+
+str3="Apna"
+str4="College"
+print(str3+str4)
+
+# length of string due to len(str)
+
+str5="AyeshaFatima"
+print(len(str5))
+str3="Apna"
+str4="College"
+final_str=str3+ " "+str4
+print(len(final_str))
+
+#  Indexing  string
+
+str="Apna college"
+print(str[0])
+print(str[4])
+
+# slicing in string index  starting index include ending not
+
+str="Apna college"
+print(str[0:3])
+print(str[0:])
+print(str[:3])
+print(str[-3:-1])
