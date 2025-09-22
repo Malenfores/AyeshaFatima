0dc585ccdc8ac6266a33e332f707bfddf69f3ad3
Malenfores <156945943+Malenfores@users.noreply.github.com>
2025-09-22 05:42:53 -0700

    display string, integers, input

---
+++ python lecture1.py
@@ -0,0 +1,12 @@
+# how to display data in python
+name="Ayesha"
+age=20
+price=25.99
+print(name,age,price)
+# how get type of variable or any number
+print(type(name))
+print(type(age))
+print(type(price))
+# how to get input in python
+name=input("Enter name:")
+print(name)
