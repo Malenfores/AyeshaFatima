2aa51504a762cd6e4770da70293bc68962cf5fdd
Malenfores <156945943+Malenfores@users.noreply.github.com>
2025-09-22 05:46:13 -0700

    string functions

---
+++ python lecture3.py
@@ -0,0 +1,18 @@
+# Pythons Functions
+#  1  endswith() return true if string ends with str
+str="I am studying python from apnacollege"
+print(str.endswith("ege"))
+print(str.endswith("pna"))
+# 2    capatilize()
+str1="i am studying python"
+print(str1.capitalize())
+str1=str1.capitalize()
+print(str1)
+# replace(old,new)  replace all occurance of old
+str2="I am a Student"
+#print(str2.replace("t","a")) 
+print(str2.replace("Student","Lawyer"))
+# find(word)  return 1st index of first occurance
+str3="I am a Student in ucp"
+print(str3.find("am"))
+print(str3.find("ucp"))
